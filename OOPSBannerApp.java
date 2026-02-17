b074eef9cee7f6c2f094785fc4dab57754b78741
Lokhareddy <lokareddy831@gmail.com>
2026-02-17 11:43:12 +0530

    Added UC@ Java implementation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,10 @@
-/**
-* OOPSBannerApp UC1 - OOPS Banner Display Application
-*
-*This class demostrates a simple java application a simple Java application   *that displays the object Oriented programming OOPS acronym to the console
-*
-*@auther Developer
-*@version 1.0
-*/
-public class OOPSBannerApp{
-public static void main (string[] args){
-system.out.println(x:OOPS);
-}
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        System.out.println(" *****     *****     *****     ***** ");
+        System.out.println("*     *   *     *   *     *   *      ");
+        System.out.println("*     *   *     *    *****     ***** ");
+        System.out.println("*     *   *     *   *              * ");
+        System.out.println(" *****     *****    *         *****  ");
+    }
 }
